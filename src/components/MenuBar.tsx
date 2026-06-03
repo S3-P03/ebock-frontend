5578cc88a63aa28c8000b7b5631c9aecb219ec72
Jeef <124377533+JeanFelixLarouche@users.noreply.github.com>
2026-06-03 16:38:41 -0400

    modification href invalide

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -46,7 +46,7 @@ export default function MenuBar({ user }: { user: User | null }) {
               variant="h6"
               noWrap
               component="a"
-              href="#app-bar-with-responsive-menu"
+              href=""
               sx={{
                 mr: 2,
                 display: "flex",
