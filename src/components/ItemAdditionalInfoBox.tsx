3c50c8038cbb320bc63e643078c43645f6dbeeb4
Jeef <124377533+JeanFelixLarouche@users.noreply.github.com>
2026-06-15 08:29:17 -0400

    Added payment options field

--- src/components/ItemAdditionalInfoBox.tsx
+++ src/components/ItemAdditionalInfoBox.tsx
@@ -36,6 +36,13 @@ export default function ItemAditionnalInfoBox({ item }: { item: DetailedItem | n
             </Box>
             <Box sx={{ width: "100%"}}>
                 <Box sx={{ display: "flex", gap: 0.5, justifyContent: "space-between", width: "100%" }}> 
+                    <Typography sx={{ fontSize: 14, color: "text.secondary" }}>Paiement </Typography>
+                    <Typography sx={{ fontSize: 14, fontWeight: 800 }}>Interac - Comptant</Typography>
+                </Box>
+                <Divider sx={{ mt: 1.5, mb: 1 }} />
+            </Box>
+            <Box sx={{ width: "100%"}}>
+                <Box sx={{ display: "flex", gap: 0.5, justifyContent: "space-between", width: "100%" }}> 
                     <Typography sx={{ fontSize: 14, color: "text.secondary" }}>Publié </Typography>
                     <Typography sx={{ fontSize: 14, fontWeight: 800 }}>{itemDate?.toLocaleDateString('fr-CA', { year: 'numeric', month: 'long', day: 'numeric' })}</Typography>
                 </Box>
