b8d1cf33a743824e71635999e07f3c7d8b597f39
Jeef <124377533+JeanFelixLarouche@users.noreply.github.com>
2026-06-15 11:33:18 -0400

    Ajout retour options de paiement et ramassage/livraison

--- src/components/ItemAdditionalInfoBox.tsx
+++ src/components/ItemAdditionalInfoBox.tsx
@@ -28,16 +28,30 @@ export default function ItemAditionnalInfoBox({ item }: { item: DetailedItem | n
                 <Divider sx={{ mt: 1.5, mb: 1 }} />
             </Box>
             <Box sx={{ width: "100%"}}>
-                <Box sx={{ display: "flex", gap: 0.5, justifyContent: "space-between", width: "100%" }}> 
-                    <Typography sx={{ fontSize: 14, color: "text.secondary" }}>Ramassage </Typography>
-                    <Typography sx={{ fontSize: 14, fontWeight: 800 }}>Campus UdeS</Typography>
+                <Box sx={{ display: "flex", gap: 0.5, justifyContent: "space-between", width: "100%" }}>
+                    <Typography sx={{ fontSize: 14, color: "text.secondary" }}>Ramassage/Livraison  </Typography>
+                    <Box sx={{ display: "flex", gap: 0.5, justifyContent: "right", width: "100%" }}>
+                        {item?.deliveryOptions && <Typography sx={{ fontSize: 14, fontWeight: 800 }}>{item?.deliveryOptions[0]}</Typography>}
+                        {item?.deliveryOptions.filter((item, idx) => {return idx!==0}).map((deliveryOption) => (
+                            <Box sx={{ display: "flex" }}>
+                                <Typography sx={{ fontSize: 14, fontWeight: 800 }}>- {deliveryOption}</Typography>
+                            </Box>
+                        ))} 
+                    </Box>
                 </Box>
                 <Divider sx={{ mt: 1.5, mb: 1 }} />
             </Box>
             <Box sx={{ width: "100%"}}>
-                <Box sx={{ display: "flex", gap: 0.5, justifyContent: "space-between", width: "100%" }}> 
-                    <Typography sx={{ fontSize: 14, color: "text.secondary" }}>Paiement </Typography>
-                    <Typography sx={{ fontSize: 14, fontWeight: 800 }}>Interac - Comptant</Typography>
+                <Box sx={{ display: "flex", gap: 0.5, justifyContent: "space-between", width: "100%" }}>
+                    <Typography sx={{ fontSize: 14, color: "text.secondary" }}>Paiement  </Typography>
+                    <Box sx={{ display: "flex", gap: 0.5, justifyContent: "right", width: "100%" }}>
+                        {item?.paymentOptions && <Typography sx={{ fontSize: 14, fontWeight: 800 }}>{item?.paymentOptions[0]}</Typography>}
+                        {item?.paymentOptions.filter((item, idx) => {return idx!==0}).map((paymentOption) => (
+                            <Box sx={{ display: "flex" }}>
+                                <Typography sx={{ fontSize: 14, fontWeight: 800 }}>- {paymentOption}</Typography>
+                            </Box>
+                        ))} 
+                    </Box>
                 </Box>
                 <Divider sx={{ mt: 1.5, mb: 1 }} />
             </Box>
