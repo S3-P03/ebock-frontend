7d3dc8316ba14a30a02fc77b4c55a22e309ac3b5
Alex Bellefroid Lefkakis <bioviruss1234@gmail.com>
2026-06-17 13:46:12 -0400

    Page login custom
    Ajout de la page login custom

--- src/keycloak-theme/login/KcPage.tsx
+++ src/keycloak-theme/login/KcPage.tsx
@@ -5,6 +5,7 @@ import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
@@ -24,6 +25,16 @@ export default function KcPage(props: { kcContext: KcContext }) {
                     case "login.ftl":
                         return (
                             <Login
+                                kcContext={kcContext}
+                                i18n={i18n}
+                                classes={classes}
+                                Template={Template}
+                                doUseDefaultCss={false}
+                            />
+                        );
+                    case "register.ftl":
+                        return (
+                             <Register
                                 kcContext={kcContext}
                                 i18n={i18n}
                                 classes={classes}
