d8a4a17fb42533873e6fa1121127c2f31b4aa23b
Jeef <124377533+JeanFelixLarouche@users.noreply.github.com>
2026-06-22 10:59:40 -0400

    Removed unused imports

--- src/pages/SellerProfile.tsx
+++ src/pages/SellerProfile.tsx
@@ -1,8 +1,6 @@
-import {
-    Box, CircularProgress,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
-import { SellerUser, SellerItem, SellerReview } from "../interfaces/Seller";
+import { SellerUser, SellerItem } from "../interfaces/Seller";
 import ProfileBox from "../components/ProfileBox";
 import ItemDisplayBox from "../components/ItemDisplayBox";
 import { useParams } from "react-router-dom";
