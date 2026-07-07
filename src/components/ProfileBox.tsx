f9d58dbe1e683730f82378e815b647d585f54d72
Jeef <124377533+JeanFelixLarouche@users.noreply.github.com>
2026-07-07 08:24:57 -0400

    EBOCK-125_TechDebt-136 (#32)

--- src/components/ProfileBox.tsx
+++ src/components/ProfileBox.tsx
@@ -5,7 +5,7 @@ import {
 import { SellerUser } from "interfaces/Seller";
 import { ReviewAverage } from "services/reviewService";
 
-export default function ProfileBox ({seller, reviewAverage}: { seller: SellerUser, reviewAverage: ReviewAverage | null }) {
+export default function ProfileBox ({seller, reviewAverage, itemsOnSaleCount}: { seller: SellerUser, reviewAverage: ReviewAverage | null, itemsOnSaleCount: number | null }) {
   return (
     <Card sx={{ p: 2.5, borderRadius: 2 }}>
       <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 1, mb: 2 }}>
@@ -15,7 +15,6 @@ export default function ProfileBox ({seller, reviewAverage}: { seller: SellerUse
         <Box sx={{ fontWeight: 700, fontSize: 16 }}>
           {seller.firstName} {seller.lastName}
         </Box>
-        <Box sx={{ fontSize: 14, color: "text.secondary" }}>Localisation</Box>
         <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
           <Rating value={reviewAverage?.avgRating ?? 0} precision={0.5} size="small" readOnly />
           <Box sx={{ fontSize: 12, color: "text.secondary" }}>({reviewAverage?.nbrReviews ?? 0})</Box>
@@ -27,11 +26,11 @@ export default function ProfileBox ({seller, reviewAverage}: { seller: SellerUse
 
       <Box sx={{ display: "flex", justifyContent: "space-between", mb: 0.5 }}>
         <Box sx={{ fontSize: 14, color: "text.secondary" }}>Articles vendus</Box>
-        <Box sx={{ fontSize: 14, fontWeight: 600 }}>{0}</Box>
+        <Box sx={{ fontSize: 14, fontWeight: 600 }}>{seller.soldItemsCount}</Box>
       </Box>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Box sx={{ fontSize: 14, color: "text.secondary" }}>Articles en vente</Box>
-        <Box sx={{ fontSize: 14, fontWeight: 600 }}>{0}</Box>
+        <Box sx={{ fontSize: 14, fontWeight: 600 }}>{itemsOnSaleCount!==null ? itemsOnSaleCount: 0}</Box>
       </Box>
     </Card>
   )
